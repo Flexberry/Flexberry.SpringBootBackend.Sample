70ae5c3dbd73e289c4297d1fd2d739b8e194a98e
Bratchikov Igor <837035+bratchikov@users.noreply.github.com>
2023-01-11 12:51:08 +0500

    Delete Docker directory

--- Docker/create-image.cmd
+++
@@ -1,2 +0,0 @@
-docker build --no-cache -f Dockerfiles/Dockerfile.Postgres -t springbootsample/postgres ../SQL
-docker build --no-cache -f Dockerfiles/Dockerfile.Backend -t springbootsample/backend ../SpringBootBackend
