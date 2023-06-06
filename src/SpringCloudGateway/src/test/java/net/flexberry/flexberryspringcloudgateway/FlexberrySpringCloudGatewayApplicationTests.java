7035ac8ccaca68a77c0b2443a24cd35135ccdab9
AntonIV8 <ananin@neoplatform.ru>
2023-06-06 16:39:59 +0500

    Добавлен проект- Spring Cloud Gateway с тестовым роутом (#35)
    Co-authored-by: AntonIV8 <anton-iv87@yandex.ru>

---
+++ src/SpringCloudGateway/src/test/java/net/flexberry/flexberryspringcloudgateway/FlexberrySpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package net.flexberry.flexberryspringcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexberrySpringCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
