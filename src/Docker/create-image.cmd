09965ef2e3100f0e95823fda9dbdfcf866f0489b
Valentin Churekov <VChurekov@neoplatform.ru>
2023-06-06 12:04:02 +0500

    feat: elasticsearch.

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -1,2 +1,3 @@
 docker build --no-cache -f Dockerfiles/Dockerfile.Postgres -t springbootsample/postgres ../SQL
 docker build --no-cache -f Dockerfiles/Dockerfile.Backend -t springbootsample/backend ../SpringBootBackend
+docker build --no-cache -f Dockerfiles/Dockerfile.ElasticSearch -t springbootsample/elasticsearch ../
