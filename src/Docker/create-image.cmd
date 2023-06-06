3a42042dfb2cc6190812c01fa756570a5ce45f18
Valentin Churekov <VChurekov@neoplatform.ru>
2023-06-06 16:14:16 +0500

    feat: grafana.

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -1,3 +1,4 @@
 docker build --no-cache -f Dockerfiles/Dockerfile.Postgres -t springbootsample/postgres ../SQL
 docker build --no-cache -f Dockerfiles/Dockerfile.Backend -t springbootsample/backend ../SpringBootBackend
 docker build --no-cache -f Dockerfiles/Dockerfile.ElasticSearch -t springbootsample/elasticsearch ../
+docker build --no-cache -f Dockerfiles/Dockerfile.Grafana -t springbootsample/grafana ../
