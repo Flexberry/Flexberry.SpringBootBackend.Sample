d2723d8e5d37a8974640b8a72866a1cbb79a469e
AntonIV8 <anton-iv87@yandex.ru>
2023-06-06 16:27:28 +0500

    Добавлен проект- Spring Cloud Gateway с тестовым роутом

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -1,2 +1,5 @@
 docker build --no-cache -f Dockerfiles/Dockerfile.Postgres -t springbootsample/postgres ../SQL
+
 docker build --no-cache -f Dockerfiles/Dockerfile.Backend -t springbootsample/backend ../SpringBootBackend
+
+docker build --no-cache -f Dockerfiles/Dockerfile.Gateway -t springbootsample/gateway ../SpringCloudGateway
