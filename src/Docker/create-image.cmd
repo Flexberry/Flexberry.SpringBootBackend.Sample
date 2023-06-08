25a88a734edd391b76f9adce0a09868900699fc2
Valentin Churekov <VChurekov@neoplatform.ru>
2023-06-08 17:39:36 +0500

    Elasticsearch+micrometer. (#41)
     feat: elasticsearc+micrometer

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -4,7 +4,7 @@ docker build --no-cache -f Dockerfiles/Dockerfile.Backend -t springbootsample/ba
 
 docker build --no-cache -f Dockerfiles/Dockerfile.Gateway -t springbootsample/gateway ../SpringCloudGateway
 
-docker build --no-cache -f Dockerfiles/Dockerfile.ElasticSearch -t springbootsample/elasticsearch ../
+docker build --no-cache -f Dockerfiles/Dockerfile.ElasticSearch -t springbootsample/elasticsearch .
 
 docker build --no-cache -f Dockerfiles/GrafanaLoki/Dockerfile.Loki -t springbootsample/loki .
 
